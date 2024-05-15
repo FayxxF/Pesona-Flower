1374c239346972d7e5708dbdab23e3801a486959
FayxxF <86724715+FayxxF@users.noreply.github.com>
2024-05-15 17:39:52 +0700

    initial backend commit

---
+++ backend/pesonaflower/src/test/java/com/clover/pesonaflower/PesonaflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clover.pesonaflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesonaflowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
